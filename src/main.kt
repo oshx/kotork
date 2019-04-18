dff03d92a9004294d54779fa3c788229b031450a
오승환 <oshxios@gmail.com>
2019-04-19 03:39:32 +0900

    혼종의 변형

--- src/main.kt
+++ src/main.kt
@@ -2,5 +2,10 @@ external fun require(module:String): dynamic
 
 fun main(args: Array<String>) {
     val j = require("jquery")
-    j("body").text("jQuery message from Kotlin code")
+    j("body")
+        .text("jQuery message from Kotlin code")
+        .append("<h1 />")
+            .find("h1")
+            .text("옛날 스타일로 추가한 제목!")
+    println("Kotlin 비중이 너무 적어서 추가...")
 }
