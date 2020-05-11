abcae1340ef4b0abdf8bb86ab2eaf72671430c02
오승환 <oshxios@gmail.com>
2020-05-12 04:08:30 +0900

    DOM 변형 경우 추가

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,12 @@
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
-    document.body?.innerText = "Kotlin overrides body!"
+    val message = "Kotlin overrides body!"
+    val body = """
+    <div class="test">$message</div>
+    <div> 위 문자열은 kotlin으로 DOM 변형을 일으킨 결과...</div>
+    <div>Template literal을 활용해서 변수를 주입시킨 결과...</div>
+    """
+    println(body)
+    document.body?.innerHTML = body
 }
